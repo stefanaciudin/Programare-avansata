63e8fb703b85c0b30ad2a5af0068b0ec0eb7cecd
stefanaciudin <100223679+stefanaciudin@users.noreply.github.com>
2023-02-21 17:26:23 +0200

    Add files via upload
    lab 1

---
+++ HelloWorld.java
@@ -0,0 +1,24 @@
+public class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Hello world!");
+        String languages[] = { "C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java" };
+        int n = (int) (Math.random() * 1000000);
+        int result = n * 3;
+        result = n + 0b10101 + 0xFF;
+        result = n * 6;
+        int sum = 0;
+        while (result > 0 || sum > 9) {
+            if (result != 0) {
+                sum = sum + result % 10;
+                result = result / 10;
+            }
+            else{
+                result = sum;
+                sum = 0;
+            }
+        }
+        System.out.println("Willy-nilly, this semester I will learn " + languages[sum]);
+
+    }
+
+}
